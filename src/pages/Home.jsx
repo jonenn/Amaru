d3e8d7f7f882131dad66b6103245349d4786fe69
Jonenn <juanmagri@outlook.es>
2025-09-20 12:40:22 -0500

    Header title and profile added to dashboard page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
 const Home = () => {
-   return <p>This is the dashboard;</p>;
+   return (
+      <div className="flex justify-between items-center">
+         <h1 className="text-2xl font-bold tracking-tightest leading-normal text-black-main">
+            Demandas Activas
+         </h1>
+         <div className="flex gap-2">
+            <div className="flex flex-col items-end gap-2 justify-center">
+               <h4 className="flex text-base font-semibold tracking-tightest leading-4 text-black-main">
+                  Jonenn Dev
+               </h4>
+               <p className="font-normal text-xs text-black-secondary tracking-tightest">
+                  Administrador
+               </p>
+            </div>
+            <div className="flex h-[43px] w-[43px] items-center justify-center bg-green-button rounded-lg">
+               <img
+                  src="https://res.cloudinary.com/dedihqcsy/image/upload/v1752815474/Jonenn_xo0ndy.gif"
+                  alt=""
+                  width={36}
+                  className="object-contain"
+               />
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Home;

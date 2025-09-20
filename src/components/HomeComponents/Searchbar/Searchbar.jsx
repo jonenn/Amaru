27f6c3653015424452c622fe57e86e72f74c0fe1
Juan J Malaver <juanmagri@outlook.es>
2025-09-20 18:45:51 -0500

    Minor visual enhancements for searchbard input

--- src/components/HomeComponents/Searchbar/Searchbar.jsx
+++ src/components/HomeComponents/Searchbar/Searchbar.jsx
@@ -8,9 +8,9 @@ const Searchbar = () => {
             id="searchbar"
             name="searchbar"
             placeholder="Buscar"
-            className="border border-gray-secondary rounded-[10px] px-3 py-2 w-1/4 bg-[url('/src/assets/search.svg')] bg-no-repeat pl-11 bg-[length:20px] bg-[position:12px_center]  placeholder-gray-tertiary text-black-search focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-tertiary bg-white"
+            className="border border-gray-secondary rounded-[10px] px-3 py-2 w-1/3 bg-[url('/src/assets/search.svg')] bg-no-repeat pl-11 bg-[length:24px] bg-[position:12px_center]  placeholder-gray-tertiary text-black-search focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-tertiary bg-white"
          />
-         <button className="flex border border-gray-secondary rounded-[10px] px-3 py-2 w-1/5 text-gray-tertiary gap-2.5 bg-white">
+         <button className="flex border border-gray-secondary rounded-[10px] px-3 py-2 w-1/4 text-gray-tertiary gap-2.5 bg-white cursor-pointer">
             <img src={Filter} alt="Filter button" width={24} />
             <p className="">Filtrar por</p>
          </button>

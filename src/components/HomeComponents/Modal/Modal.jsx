7a56bd92b57af41649844ac60b5c0443b1ba2ab3
Jonenn <juanmagri@outlook.es>
2025-09-21 15:32:20 -0500

    Download button added to modal

--- src/components/HomeComponents/Modal/Modal.jsx
+++ src/components/HomeComponents/Modal/Modal.jsx
@@ -1,18 +1,24 @@
 import { DemandBody } from "@/components/HomeComponents/DemandBody";
 import { ModalHeader } from "./ModalHeader";
 import { Tag } from "@/components/HomeComponents/Tag";
+import File from "./File";
 
 const Modal = ({
-   content: { name, demandType, status, client, description },
+   content: { name, demandType, status, client, description, documents },
 }) => {
    return (
-      <div to="#" className="flex flex-col bg-white gap-2.5">
+      <div to="#" className="flex flex-col bg-white gap-2.5 h-full">
          <ModalHeader title={name} />
          <div className="flex flex-wrap gap-2.5">
             <Tag label={demandType} />
             <Tag label={status} />
          </div>
-         <DemandBody customer={client} description={description} />
+         <div className="flex flex-col justify-between h-full">
+            <DemandBody customer={client} description={description} />
+            <div className="flex flex-col gap-2">
+               <File documents={documents} />
+            </div>
+         </div>
       </div>
    );
 };

711bdc841c1d11aa25978feb06704adcfa9d6457
Jonenn <juanmagri@outlook.es>
2025-09-21 16:29:46 -0500

    Animation added to primary button

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -2,9 +2,24 @@ import Plus from "@/assets/plus.svg";
 
 const PrimaryButton = ({ children }) => {
    return (
-      <button className="flex bg-green-button hover:bg-black text-white-secondary justify-center gap-2 rounded-[10px] px-6 py-2 cursor-pointer w-1/2 font-semibold">
+      <button
+         className="
+            group flex items-center justify-center gap-2
+            bg-green-button hover:bg-black text-white-secondary
+            rounded-[10px] px-6 py-2 cursor-pointer w-1/2 font-semibold
+            transition-transform duration-300 ease-out
+         "
+      >
          {children}
-         <img src={Plus} alt={children} />
+         <img
+            src={Plus}
+            alt={children}
+            className="
+            w-5 h-5
+            transition-transform duration-300 ease-out
+            group-hover:rotate-90
+            "
+         />
       </button>
    );
 };

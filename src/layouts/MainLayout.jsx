7fda7b70e1c65b5ceafe93ef3d3d1731b007d2bb
Juan J Malaver <juanmagri@outlook.es>
2025-09-22 21:18:02 -0500

    Hotifx for sidebar tooltips

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
-import SideMenu from "@/components/SideMenu/SideMenu";
-import MobileMenu from "@/components/MobileMenu/MobileMenu";
+import { Outlet } from 'react-router-dom';
+import SideMenu from '@/components/SideMenu/SideMenu';
+import MobileMenu from '@/components/MobileMenu/MobileMenu';
 
 const MainLayout = () => {
    return (
       <div className="flex flex-col md:flex-row relative">
-         <SideMenu className="hidden md:flex md:flex-col fixed bottom-0 top-0 max-h-screen" />
+         <SideMenu className="hidden md:flex md:flex-col fixed bottom-0 top-0 max-h-screen z-40" />
          <MobileMenu className="flex md:hidden py-4.5 px-3 gap-3 justify-between" />
          <div className="w-full bg-white-main md:rounded-l-2xl min-h-screen md:ml-[63px]">
             <Outlet />
